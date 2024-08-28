fa917fa7928cb59842028d895de83abff825e850
BORA <a52447879@gmail.com>
2024-08-28 19:06:06 +0900

    feat: Add ReadOnlyText widget for displaying read-only text

--- lib/widget/text.dart
+++ lib/widget/text.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
+// 섹션 테스트
 class SectionText extends StatelessWidget {
   final String text;
   final Color textColor;
@@ -13,6 +13,33 @@ class SectionText extends StatelessWidget {
       text,
       style: TextStyle(
           color: textColor, fontSize: 20, fontWeight: FontWeight.bold),
+    );
+  }
+}
+
+// 읽기 전용 텍스트
+class ReadOnlyText extends StatelessWidget {
+  final String title;
+  const ReadOnlyText({super.key, required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      decoration: ShapeDecoration(
+        color: Colors.white,
+        shape: RoundedRectangleBorder(
+          side: const BorderSide(width: 2),
+          borderRadius: BorderRadius.circular(4),
+        ),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Text(
+          title,
+          style: const TextStyle(fontSize: 16, color: Colors.black),
+        ),
+      ),
     );
   }
 }

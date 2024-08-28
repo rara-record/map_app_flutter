0d0e517dd36f9aed526b9b1247bc4142739c9159
BORA <a52447879@gmail.com>
2024-08-29 00:10:01 +0900

    feat: Add favorite icon to SearchResultScreen for users to mark stores as favorites

--- lib/screen/search_result.dart
+++ lib/screen/search_result.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:map_app/model/favorite.dart';
 import 'package:map_app/model/food_store.dart';
 import 'package:map_app/widget/appbar.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 // 검색 결과 화면
 class SearchResultScreen extends StatefulWidget {
   final List<FoodStoreModel> listFoodStore;
+
   const SearchResultScreen({super.key, required this.listFoodStore});
 
   @override
@@ -12,15 +15,79 @@ class SearchResultScreen extends StatefulWidget {
 }
 
 class _SearchResultScreenState extends State<SearchResultScreen> {
+  final supabase = Supabase.instance.client;
+  List<FavoriteModel> listFavorite = []; // 즐겨찾기 정보들
+
+  @override
+  void initState() {
+    _getMyFavorite();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      appBar: CommonAppBar(title: '검색 결과', isLeading: true),
-      body: Center(
-        child: Column(
-          children: [],
-        ),
+    return Scaffold(
+      appBar: const CommonAppBar(title: '검색 결과', isLeading: true),
+      body: widget.listFoodStore.isNotEmpty
+          ? ListView.builder(
+              itemCount: widget.listFoodStore.length,
+              itemBuilder: (context, index) {
+                FoodStoreModel foodStoreModel = widget.listFoodStore[index];
+                return _buildListItemFoodStore(foodStoreModel);
+              },
+            )
+          : const Text('검색 결과가 없습니다.'),
+    );
+  }
+
+  // 맛집 정보 리스트 아이템 생성
+  Widget _buildListItemFoodStore(FoodStoreModel foodStoreModel) {
+    return SizedBox(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            children: [
+              Text(foodStoreModel.storeName),
+              _buildFavoriteIcon(foodStoreModel),
+            ],
+          ),
+        ],
       ),
     );
+  }
+
+  // 찜하기 여부 아이콘 생성
+  _buildFavoriteIcon(FoodStoreModel foodStoreModel) {
+    bool isFavorite = false;
+
+    for (FavoriteModel favoriteModel in listFavorite) {
+      // 찜하기 이력 검사
+      if (favoriteModel.id == foodStoreModel.id) {
+        isFavorite = true;
+        break;
+      }
+    }
+
+    if (!isFavorite) {
+      return const Icon(Icons.star_border_outlined, size: 32);
+    } else {
+      return const Icon(Icons.star, size: 32);
+    }
+  }
+
+  Future<void> _getMyFavorite() async {
+    if (widget.listFoodStore.isEmpty) return;
+
+    // 현재 내 찜하기 이력 가져오기
+    final myFavoriteMap = await supabase
+        .from('favorite')
+        .select()
+        .eq('favorite_uid', supabase.auth.currentUser!.id);
+
+    setState(() {
+      listFavorite =
+          myFavoriteMap.map((e) => FavoriteModel.fromJSON(e)).toList();
+    });
   }
 }
